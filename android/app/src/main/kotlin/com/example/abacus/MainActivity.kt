b46d066f0064fd69f889bcfdacdb46383e498951
Sumit Basak <basaksum@umich.edu>
2023-02-11 21:39:42 -0500

    Package name change

--- android/app/src/main/kotlin/com/example/abacus/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.abacus
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/mittsq/abacus/MainActivity.kt
@@ -0,0 +1,6 @@
+package mittsq.abacus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
